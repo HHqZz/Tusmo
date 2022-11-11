513d923524d39ae7f9c9fc81b92bb0b6305f95e1
HHqZz <cbouis@outlook.fr>
2022-11-11 17:03:55 -0500

    super menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void onClickPlayButton(){
+        Debug.Log("TouchedRGFSGFSRFGSGE ");    
+        SceneManager.LoadScene("TusmoGame");
+    }
+}
